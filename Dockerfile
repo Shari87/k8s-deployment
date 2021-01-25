a4c5bd51ee4ad734f6a40dada656117c00962e34
Shari87 <sahajhaksh@gmail.com>
2021-01-25 23:11:23 +0530

    my initial commit

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM php:apache
-COPY index.php /var/www/html
-EXPOSE 80
--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>Display hello world</title>
- </head>
- <body>
-    <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
